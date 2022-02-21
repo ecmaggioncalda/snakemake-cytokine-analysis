2a08bdd7cb551c08e2aa32d4c92f547313e9d68e
ecmaggioncalda <emilycma@umich.edu>
2022-02-21 14:02:38 -0500

    increase memory requirements for batch submission, remove unnecessary snakemake submission commands

--- code/submit_slurm.sh
+++ code/submit_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=100MB
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --time=96:00:00
 
 #SBATCH --output=log/hpc/slurm-%j_%x.out
@@ -16,4 +16,4 @@
 #SBATCH --mail-user=emilycma@umich.edu
 #SBATCH --mail-type=BEGIN,END
 
-time snakemake --profile config/slurm --latency-wait 90 --configfile config/config.yml --show-failed-logs --verbose --keep-incomplete
+time snakemake --profile config/slurm --latency-wait 90 --configfile config/config.yml --restart-times 5
