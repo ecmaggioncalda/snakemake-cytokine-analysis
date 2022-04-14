80e38758bb8a1eee216878d8a15a4dfb88d2cf7a
ecmaggioncalda <emilycma@umich.edu>
2022-04-14 09:14:01 -0400

    add summary functions, change to drop near-zero variance variants not just zero-variance variants

--- code/preproc.R
+++ code/preproc.R
@@ -3,11 +3,15 @@ library(mikropml)
 
 doFuture::registerDoFuture()
 future::plan(future::multicore, workers = snakemake@resources[["ncores"]])
+options(future.globals.maxSize = Inf)
 
 data_raw <- readr::read_csv(snakemake@input[["csv"]])
+dim(data_raw)
+
 data_processed <- preprocess_data(data_raw,
                                   outcome_colname = snakemake@params[['outcome_colname']],
                                   group_neg_corr = TRUE,
-                                  remove_var = 'zv')
+                                  remove_var = 'nzv')
+summary(data_processed)
 
 saveRDS(data_processed, file = snakemake@output[["rds"]])
